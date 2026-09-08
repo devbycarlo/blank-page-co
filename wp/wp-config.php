3fd8510637033b9797a9b8827d74f5dec432a26e
designbycarlo <37431817+designbycarlo@users.noreply.github.com>
2026-09-08 16:23:02 +0800

    revert: disable WP_DEBUG after verifying site works

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -97,9 +97,7 @@ $table_prefix = isset($_ENV['TABLE_PREFIX']) ? $_ENV['TABLE_PREFIX'] : 'wp_';
  *
  * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_LOG', true );
-define( 'WP_DEBUG_DISPLAY', true );
+define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
